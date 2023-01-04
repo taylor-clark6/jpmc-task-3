9118ec2bb1af481be7b01bbe54392cf37fe3c765
Taylor C <tclark38@fordham.edu>
2023-01-04 17:27:11 -0500

    Fixed syntax error

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -15,8 +15,8 @@ export interface Row {
 export class DataManipulator {
   //updated the generateRow method to properly process raw server data
   static generateRow(serverRespond: ServerRespond[]): Row {
-    const priceABC = (serverRespond[0].top_ask_price + serverRespond[0].top_bid_price) / 2;
-    const priceDEF = (serverRespond[1].top_ask_price + serverRespond[1].top_bid_price) / 2;
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.1;
     const lowerBound = 1 - 0.1;
